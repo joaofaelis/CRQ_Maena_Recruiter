e1a0caaa0a19b3e819411fd5eb8a87840066b96d
João Faelis <joaofaelis@hotmail.com>
2025-03-20 07:51:01 -0300

    Infrastructure

---
+++ src/infrastructure/SQL/main.py
@@ -0,0 +1,50 @@
+import pyodbc
+from decouple import config
+
+class InfrastructureSQL:
+
+    def __init__(self):
+        self.server = config('SERVER')
+        self.database = config('DATABASE')
+        self.username = config('USERNAME')
+        self.password = config('PASSWORD')
+        self.UID = config('UID')
+        self.conn = None
+
+    def connect(self):
+        try:
+            conn_str = (
+                f'DRIVER={{ODBC Driver 18 for SQL Server}};'
+                f'SERVER=tcp:{self.server},1433;'
+                f'DATABASE={self.database};'
+                f'UID={self.UID};'
+                f'PWD={self.password};'
+                f'Encrypt=yes;'
+                f'TrustServerCertificate=no;'
+                f'Connection Timeout=30;'
+            )
+            self.conn = pyodbc.connect(conn_str)
+        except Exception as e:
+            print(f"Erro ao conectar ao SQL Server: {e}")
+            self.conn = None
+
+    def close_connection(self):
+        try:
+            if self.conn:
+                self.conn.close()
+        except Exception as e:
+            print(f"Erro ao fechar conexão: {e}")
+
+    def cursor_db(self):
+        self.connect()
+        if self.conn:
+            return self.conn.cursor()
+        else:
+            return None
+
+
+if __name__ == "__main__":
+    db = InfrastructureSQL()
+    cursor = db.cursor_db()
+    print('Conected')
+
