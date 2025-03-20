33d291955b9449f77c6617953ebdc79030f829ad
João Faelis <joaofaelis@hotmail.com>
2025-03-20 08:57:48 -0300

    Infrastructure

--- src/infrastructure/SQL/main.py
+++ src/infrastructure/SQL/main.py
@@ -2,28 +2,43 @@ import pyodbc
 from decouple import config
 
 class InfrastructureSQL:
-
     def __init__(self):
         self.server = config('SERVER')
         self.database = config('DATABASE')
-        self.username = config('USERNAME')
-        self.password = config('PASSWORD')
-        self.UID = config('UID')
+        self.use_trusted_connection = config('USE_TRUSTED_CONNECTION', default="false").lower() == "true"
+        self.username = config('USERNAME', default="")
+        self.password = config('PASSWORD', default="")
+        self.UID = config('UID', default="")
+
         self.conn = None
 
     def connect(self):
         try:
-            conn_str = (
-                f'DRIVER={{ODBC Driver 18 for SQL Server}};'
-                f'SERVER=tcp:{self.server},1433;'
-                f'DATABASE={self.database};'
-                f'UID={self.UID};'
-                f'PWD={self.password};'
-                f'Encrypt=yes;'
-                f'TrustServerCertificate=no;'
-                f'Connection Timeout=30;'
-            )
+            if self.use_trusted_connection:
+                # Autenticação Windows (Trusted Connection)
+                conn_str = (
+                    f'DRIVER={{ODBC Driver 18 for SQL Server}};'
+                    f'SERVER={self.server};'
+                    f'DATABASE={self.database};'
+                    f'TRUSTED_CONNECTION=yes;'
+                    f'Encrypt=no;'  # 🔴 Desativando criptografia para evitar erro de certificação
+                )
+            else:
+                # Autenticação com usuário e senha
+                conn_str = (
+                    f'DRIVER={{ODBC Driver 18 for SQL Server}};'
+                    f'SERVER=tcp:{self.server},1433;'
+                    f'DATABASE={self.database};'
+                    f'UID={self.UID};'
+                    f'PWD={self.password};'
+                    f'Encrypt=yes;'
+                    f'TrustServerCertificate=no;'
+                    f'Connection Timeout=30;'
+                )
+
             self.conn = pyodbc.connect(conn_str)
+            print("Conexão estabelecida com sucesso!")
+
         except Exception as e:
             print(f"Erro ao conectar ao SQL Server: {e}")
             self.conn = None
@@ -46,5 +61,4 @@ class InfrastructureSQL:
 if __name__ == "__main__":
     db = InfrastructureSQL()
     cursor = db.cursor_db()
-    print('Conected')
 
