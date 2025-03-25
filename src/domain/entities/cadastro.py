79acf1839164ebb3e64506c78ef833db930d0b99
João Faelis <joaofaelis@hotmail.com>
2025-03-25 10:52:37 -0300

    correção entidades

--- src/domain/entities/cadastro.py
+++ src/domain/entities/cadastro.py
@@ -20,9 +20,33 @@ class Cadastro(BaseModel):
     nome_recrutador: Optional[str]
     digitador: Optional[str]
     id: Optional[int] = None
+    carimbo_data_hora: datetime
 
     class Config:
         from_attributes = True  # Alteração aqui de 'orm_mode' para 'from_attributes'
 
     def __repr__(self):
         return f"<Cadastro {self.nome_completo} - {self.cpf}>"
+class AtualizarCadastro(BaseModel):
+    # Somente os campos que podem ser atualizados
+    nome_completo: Optional[str]
+    telefone: Optional[str]
+    email: Optional[str]
+    estado: Optional[str]
+    bairro: Optional[str]
+    sexo: Optional[str]
+    idade: Optional[int]
+    assunto: Optional[str]
+    data_participacao: Optional[datetime]
+    metodologia: Optional[str]
+    cliente: Optional[str]
+    classe_social: Optional[str]
+    ocupacao: Optional[str]
+    nome_recrutador: Optional[str]
+    digitador: Optional[str]
+
+    class Config:
+        from_attributes = True  # Alteração aqui de 'orm_mode' para 'from_attributes'
+
+    def __repr__(self):
+        return f"<AtualizarCadastro {self.nome_completo} - {self.cpf}>"
